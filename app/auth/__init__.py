1993dd3eed33f335b6d74844b372ec0ceb27dccd
crunux <joancruz0502@hotmail.com>
2024-09-08 19:41:56 -0400

    refactor: change crendentials methods auth to BearerHTTP

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -1,9 +1,8 @@
-import secrets
 import jwt
 from datetime import datetime, timedelta, timezone
 from fastapi import Depends, HTTPException, status, Security
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm, HTTPBasic, HTTPBasicCredentials
-from jwt.exceptions import InvalidTokenError, ExpiredSignatureError
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+from jwt.exceptions import InvalidTokenError
 from sqlmodel import Session, select
 from typing import Annotated
 from pydantic import ValidationError
@@ -14,36 +13,46 @@ from app.config import settings
 from app.models.users import User
 
 
-basic_auth = HTTPBasic(auto_error=False)
+# basic_auth = HTTPBasic(auto_error=False)
 
 
 ALGORITHM = "HS256"
 
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token", scopes={
-                                     "me": "Read information about the current user.", "items": "Read items."})
+bearer_auth = HTTPBearer(auto_error=False)
 
 
-def authent(credentials: HTTPBasicCredentials = Depends(basic_auth)):
-    if check_auth_creds(credentials):
-        return True
+# def authent(credentials: HTTPBasicCredentials = Depends(basic_auth)):
+#     return True
+#     # if check_auth_creds(credentials):
+#     #     return True
+#     # raise HTTPException(
+#     #     status_code=status.HTTP_401_UNAUTHORIZED,
+#     #     detail="Incorrect user or password",
+#     #     headers={"WWW-Authenticate": "Basic"},
+#     # )
+
+
+# def check_auth_creds(credentials: HTTPBasicCredentials = Depends(basic_auth)):
+#     correct_username = secrets.compare_digest(
+#         credentials.username, settings.API_USERNAME)
+#     correct_password = secrets.compare_digest(
+#         credentials.password, settings.API_PASSWORD)
+
+#     if correct_username and correct_password:
+#         return True
+
+#     return False
+
+
+def get_authorization_header(bearer: HTTPAuthorizationCredentials = Depends(bearer_auth)) -> str:
+    if not bearer or bearer.scheme != "Bearer":
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate credentials", headers={
+            "WWW-Authenticate": "Bearer"},)
+    if bearer and bearer.credentials:
+        return bearer.credentials
     raise HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Incorrect user or password",
-        headers={"WWW-Authenticate": "Basic"},
-    )
-
-
-def check_auth_creds(credentials: HTTPBasicCredentials = Depends(basic_auth)):
-    correct_username = secrets.compare_digest(
-        credentials.username, settings.API_USERNAME)
-    correct_password = secrets.compare_digest(
-        credentials.password, settings.API_PASSWORD)
-
-    if correct_username and correct_password:
-        return True
-
-    return False
+        status_code=status.HTTP_401_UNAUTHORIZED, detail="Unauthorized")
 
 
 def create_token(data: dict, expires_delta: timedelta | None = None) -> str:
@@ -58,7 +67,7 @@ def create_token(data: dict, expires_delta: timedelta | None = None) -> str:
     return encoded_jwt
 
 
-def get_current_user(token: Annotated[str, Depends(oauth2_scheme)], db: Session = Depends(get_session)) -> dict:
+def get_current_user(token: Annotated[str, Depends(get_authorization_header)], db: Session = Depends(get_session)) -> dict:
     credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate credentials", headers={
                                           "WWW-Authenticate": "Bearer"},)
     try:
@@ -67,12 +76,11 @@ def get_current_user(token: Annotated[str, Depends(oauth2_scheme)], db: Session 
         email: str | None = payload.get("sub")
         if email is None:
             raise credentials_exception
-        token_scopes = payload.get("scopes", [])
-        token_data = TokenData(scopes=token_scopes, email=email)
+        token_data = TokenData(email=email)
     except (InvalidTokenError, ValidationError):
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN, detail="Invalid token")
-    user = db.exec(select(User).where(User.email == email)).first()
+    user = db.exec(select(User).where(User.email == token_data.email)).first()
     if user is None:
         raise credentials_exception
     return user
