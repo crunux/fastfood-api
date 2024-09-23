17cdc836a74a8503391c33a7f67464765eef401c
crunux <joancruz0502@hotmail.com>
2024-09-22 20:35:18 -0400

    refactor: add multipart form to router for upload image

--- app/routers/products.py
+++ app/routers/products.py
@@ -1,5 +1,6 @@
+from decimal import Decimal
 from typing import Annotated
-from fastapi import APIRouter, Depends, UploadFile
+from fastapi import APIRouter, Depends, File, Form, UploadFile, status
 from sqlmodel import Session
 import uuid
 from app.auth import get_current_active_admin, get_current_active_user
@@ -9,13 +10,16 @@ from app.controllers import create_product, get_products, get_product_by_id, upd
 from app.models.users import UserInDB
 
 
-router = APIRouter(tags=["Products"], responses={
+router = APIRouter(responses={
                    404: {"description": "Not found"}})
 
 
-@router.post("", response_model=ProductInDB)
-async def create_a_product(newProduct: ProductCreate, userAccess: Annotated[UserInDB,  Depends(get_current_active_admin)], file: UploadFile  ,db: Session = Depends(get_session)) -> ProductInDB:
-    return await create_product(newProduct, file, db)
+# async def create_a_product(newProduct: ProductCreate, userAccess: Annotated[UserInDB,  Depends(get_current_active_admin)], db: Session = Depends(get_session)) -> ProductInDB:
+@router.post("", response_model=ProductInDB, status_code=status.HTTP_201_CREATED)
+async def create_a_product(name: Annotated[str, Form()], description: Annotated[str, Form()], price: Annotated[Decimal, Form()], category_id: Annotated[uuid.UUID, Form()], image: Annotated[UploadFile, File()], userAccess: Annotated[UserInDB,  Depends(get_current_active_admin)], db: Session = Depends(get_session)) -> ProductInDB:
+    print(image, "imagen router")
+    product = ProductCreate(name=name, description=description, price=price, category_id=category_id, active=True)
+    return await create_product(product, image, db)
 
 
 @router.get("", response_model=list[ProductInDB])
