aa8b577c9372d38bca85cc1f325b22c1a8ff40aa
crunux <joancruz0502@hotmail.com>
2024-10-15 20:59:19 -0400

    chore: add host local

--- server.py
+++ server.py
@@ -5,4 +5,4 @@ from app.config import settings
 api = create_app(settings)
 
 if __name__ == "__main__":
-    uvicorn.run("server:api", port=5000, log_level='info', reload=True)
+    uvicorn.run("server:api", host='127.0.0.1' ,port=5000, log_level='info', reload=True)
